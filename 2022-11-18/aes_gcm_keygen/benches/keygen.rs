0884aa11056633f4750df5975e607b66e93a2974
Arnaud Brousseau <arnaud.brousseau@gmail.com>
2022-12-24 10:03:11 -0600

    Cargo fmt

--- 2022-11-18/aes_gcm_keygen/benches/keygen.rs
+++ 2022-11-18/aes_gcm_keygen/benches/keygen.rs
@@ -2,9 +2,7 @@ use aes_gcm_keygen::{ring_aes_gcm_keygen, rust_crypto_aes_gcm_keygen};
 use criterion::{criterion_group, criterion_main, Criterion};
 
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("Ring's AES-GCM keygen", |b| {
-        b.iter(ring_aes_gcm_keygen)
-    });
+    c.bench_function("Ring's AES-GCM keygen", |b| b.iter(ring_aes_gcm_keygen));
     c.bench_function("RustCrypto's AES-GCM keygen", |b| {
         b.iter(rust_crypto_aes_gcm_keygen)
     });
