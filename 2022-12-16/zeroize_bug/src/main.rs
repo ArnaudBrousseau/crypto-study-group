fc46b8ac02fedc6dde4de4889e0e81e683752839
Arnaud Brousseau <arnaud.brousseau@gmail.com>
2023-01-09 22:59:32 -0600

    Commit zeroize_bug project

---
+++ 2022-12-16/zeroize_bug/src/main.rs
@@ -0,0 +1,29 @@
+use zeroize::Zeroize;
+
+#[derive(Debug)]
+struct KeyPair {
+    public_key: [u8; 4],
+    private_key: [u8; 4],
+}
+
+impl Drop for KeyPair {
+    fn drop(&mut self) {
+        self.private_key.zeroize();
+        self.public_key.zeroize();
+    }
+}
+
+fn main() {
+    let key_pair = KeyPair {
+        public_key: *b"PUBL",
+        private_key: *b"PRIV",
+    };
+    let secret_mem_locus = key_pair.private_key.as_ptr();
+    println!("Secret locus: {:p}", secret_mem_locus);
+    drop(key_pair);
+    println!("Keypair erased...or is it?");
+
+    println!("Reading secret locus: {:?}", unsafe {
+        String::from_utf8_unchecked(core::slice::from_raw_parts(secret_mem_locus, 4).to_vec())
+    });
+}
