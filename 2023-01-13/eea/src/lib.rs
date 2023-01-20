70ac79fabc568b50b2a38b775e4e0f11fa6771ae
Arnaud Brousseau <arnaud.brousseau@gmail.com>
2023-01-20 01:27:14 -0600

    cargo fmt

--- 2023-01-13/eea/src/lib.rs
+++ 2023-01-13/eea/src/lib.rs
@@ -6,7 +6,7 @@ pub fn eea(a: i32, b: i32) -> (i32, i32, i32) {
     if a == 0 || b == 0 {
         return (-1, -1, -1);
     }
-    
+
     if a > b {
         compute_eea(b, a, 1, 0, 0, 1)
     } else {
@@ -18,12 +18,12 @@ pub fn eea(a: i32, b: i32) -> (i32, i32, i32) {
 /// For each round, a < b, and we iterate on a, b, ua/va, and ub/vb
 fn compute_eea(a: i32, b: i32, ua: i32, va: i32, ub: i32, vb: i32) -> (i32, i32, i32) {
     if a == 0 {
-        return (b, ub, vb)
+        return (b, ub, vb);
     }
-    let quotient = b/a;
-    let remainder = b - quotient*a;
+    let quotient = b / a;
+    let remainder = b - quotient * a;
 
-    compute_eea(remainder, a, ub-quotient*ua, vb-quotient*va, ua, va)
+    compute_eea(remainder, a, ub - quotient * ua, vb - quotient * va, ua, va)
 }
 
 #[cfg(test)]
@@ -35,7 +35,7 @@ mod test {
         assert_eq!(eea(0, 0), (-1, -1, -1));
         assert_eq!(eea(0, 5), (-1, -1, -1));
         assert_eq!(eea(10, 10), (10, 1, 0));
-        
+
         // Calculated manually
         assert_eq!(eea(397, 2357), (1, -754, 127));
         // Ordering shouldn't matter
