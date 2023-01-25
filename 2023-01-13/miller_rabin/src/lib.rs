25748adf47b140c5881bd0172e2f3af1c5ef059c
Arnaud Brousseau <arnaud.brousseau@gmail.com>
2023-01-25 17:52:04 -0600

    cargo fmt

--- 2023-01-13/miller_rabin/src/lib.rs
+++ 2023-01-13/miller_rabin/src/lib.rs
@@ -34,7 +34,7 @@ pub fn miller_rabin(bytes: Vec<u8>) -> Result<bool, MillerRabinError> {
     // Pad `bytes`, which may or may not be shorter than 1,024 bytes
     let padded: [u8; REQUIRED_BYTE_LENGTH_FOR_U8192] = {
         let mut padded = [0u8; REQUIRED_BYTE_LENGTH_FOR_U8192];
-        padded[REQUIRED_BYTE_LENGTH_FOR_U8192-bytes.len()..].copy_from_slice(&bytes);
+        padded[REQUIRED_BYTE_LENGTH_FOR_U8192 - bytes.len()..].copy_from_slice(&bytes);
         padded
     };
 
